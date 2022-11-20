38de447b57873ae3a403e30cb5692d55bbe5f8c5
Olesia Leonidova <pozitifchick@gmail.com>
2022-11-20 18:34:30 +0100

    add Example 005 Condition if-Else

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Insert your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+Console.WriteLine("So great! It's Maria!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
