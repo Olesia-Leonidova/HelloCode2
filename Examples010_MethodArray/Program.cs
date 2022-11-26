5c22b1f136f930cf1575d8963cb143ad1f6d6476
Olesia Leonidova <pozitifchick@gmail.com>
2022-11-26 20:37:58 +0100

    Add Examples 008-011

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[]array = {111, 22, 3, 73, 24, 85, 56, 73, 18};
+
+int n = array.Length;
+int find = 73;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;//если хочу чтобы найдя первый индекс, больше не искал
+    }
+    //index = index + 1;
+    index++;
+}
---
+++ Examples011_ArrayLibrary/Program.cs
@@ -0,0 +1,54 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index+1
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //Вместо 0 ставим -1, это такой
+                       //искуственный приём, на случай если элемента
+                       // нет, таким образом если искать несуществующий эл-т увидим -1
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+    
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4; //принудительно добавили 4 на 4 позицию
+array[6] = 4; //принудительно добавили 4 на 6 позицию
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
